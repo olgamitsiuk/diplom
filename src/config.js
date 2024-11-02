7bb0cad0e3f6499383f8dce3a76057c38fec19a4
Mitsiuk Olha <mitsyuk.olga@gmail.com>
2024-11-02 00:13:10 +0000

    add env variables

--- src/config.js
+++ src/config.js
@@ -1,5 +1,16 @@
-const API_URL = "http://localhost:3001/api/";
-const BASE_URL = "http://localhost:3001";
+import dotenv from 'dotenv';
+
+// Загружаем переменные окружения
+dotenv.config({
+    path: process.env.NODE_ENV === 'production'
+        ? '.env.production'
+        : '.env.development'
+});
+
+const API_URL = process.env.API_URL || "/api/";
+
+const BASE_URL = process.env.BASE_URL || "";
+
 export {
     API_URL,
     BASE_URL
